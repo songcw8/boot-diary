5ffe8250f92025136d2271a1a858c34f4ad5a1b8
songcw8 <ben990158@gmail.com>
2025-04-17 09:15:52 +0900

    entity

---
+++ src/main/java/org/example/bootdiary/model/entity/Article.java
@@ -0,0 +1,27 @@
+package org.example.bootdiary.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Data
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    private String filename;
+    // DB 들어갈 때는 UTC로 하자
+    @CreatedDate
+    private LocalDateTime createdAt = LocalDateTime.ofInstant(Instant.now(), ZoneOffset.UTC);
+}
