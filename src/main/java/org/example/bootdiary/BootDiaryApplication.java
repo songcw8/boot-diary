9a5f6f3a7db821b8f46f1744df601403937fb8da
songcw8 <ben990158@gmail.com>
2025-04-17 09:15:52 +0900

    Initial commit

---
+++ src/main/java/org/example/bootdiary/BootDiaryApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDiaryApplication.class, args);
+    }
+
+}
